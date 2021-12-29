c27be0a5f134715079c2004038fa480ba0bd3b6e
NathanielHab <46847625+NathanielHab@users.noreply.github.com>
2021-12-29 16:49:12 -0500

    Added functionality
    added isImpossible and isStuck

--- Sudoku.py
+++ Sudoku.py
@@ -75,7 +75,44 @@ def isDone(S):
                 return False
     return True
 
+def isStuck(S):
+    '''returns true if sudoku S is stuck (no obvious moves)'''
+    if isDone(S):
+        return False
+    t = 0
+    while (t < 81):  #try t amount of times to see if we're stuck
+        r = 0
+        while r < 9:
+            i = 0
+            while i < 9:
+                if S[r][i] == 0:
+                    n = 1
+                    ans = []
+                    while n < 10:
+                        if not (inRow(n, S[r]) or inCol(n, Col(i+1, S)) or inRow(n, Box(whichBox(r, i), S))):
+                            ans.append(n)  #potential nums in this square
+                        n+=1
+                    if len(ans) == 1:
+                        return False  #we are not stuck
+                i+=1
+            r+=1
+        return True  #we are stuck
+
+def isImpossible(S, maybes):
+    '''returns true if sudoku S cannot be completed'''
+    #print("checking impossible")
+    r = 0
+    while r < 9:
+        i = 0
+        while i < 9:
+            if len(maybes[r][i]) == 0:
+                return True
+            i+=1
+        r+=1
+    return False
+
 def formatPrint(S):
+    '''prints the sudoku board in a nice, readable format'''
     print("-------------------------------------")
     for r in range(9):
         print("", end='| ')
@@ -105,13 +142,15 @@ def removeMaybesRow(n, m, r, i, j, maybes):
 
 def solve(S, maybes):
     lastS = []
-    while (not isDone(S)):
+    while ((not isDone(S)) and (not isStuck(S))):
         r = 0
         lastS = S.copy()
         while r < 9:
             i = 0
             while i < 9:
-                if S[r][i] == 0:
+                if S[r][i] != 0:
+                    maybes[r][i] = [S[r][i]]
+                elif S[r][i] == 0:
                     n = 1
                     ans = []
                     while n < 10:
@@ -134,18 +173,45 @@ def solve(S, maybes):
                             j+=1
                 i+=1
             r+=1
-        #if S == lastS: #no progress, so make a guess and check if correct
-            
-        #formatPrint(S)
-    #print(str(maybes))
-    formatPrint(S)
+    if isImpossible(S, maybes):
+        print("impossible")
+        return []  #indicates S is impossible
+    if isStuck(S): #no progress, so make a guess and check if correct
+        print("gonna guess")
+        r = 0
+        while r < 9:
+            i = 0
+            while i < 9:
+                if len(maybes[r][i]) == 2: #find first square in S that has only 2 possible inputs
+                    mG1 = maybes.copy()
+                    mG1[r][i] = [mG1[r][i][0]]
+                    sG1 = solve(S, mG1)  #guess one
+                    if len(sG1) != 0:
+                        return sG1
+                    mG2 = maybes.copy()
+                    mG2[r][i] = [mG2[r][i][1]]
+                    sG2 = solve(S, mG2)  #guess two
+                    return sG2  #don't need to check if impossible since if we are here then sG1 is impossible
+                        
+                
+    return S
+    #formatPrint(S)
 
 
+
+#easy one
 S = [[8,1,7,0,0,0,0,4,5],[0,0,0,0,5,1,7,0,6],[2,6,5,0,0,3,0,0,1],[4,7,0,5,6,8,0,0,0],[9,5,1,0,0,0,0,8,0],[0,3,0,0,9,0,2,0,0],[0,4,0,2,0,0,0,0,0],[0,0,0,0,0,5,0,7,9],[5,8,9,7,3,0,1,6,0]]
+
+#hard one (does not work yet)
 #S = [[5,0,0,0,0,1,0,4,0],[8,0,0,7,4,0,5,0,0],[1,0,0,3,8,0,9,0,0],[0,0,2,0,0,4,0,0,0],[6,0,0,0,3,0,0,0,1],[0,0,0,6,0,0,4,0,0],[0,0,8,0,7,9,0,0,4],[0,0,7,0,2,3,0,0,6],[0,3,0,4,0,0,0,0,2]]
+
+#initialise list of possible numbers in each square
 maybes = [[[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[],[]]]
-#print(str(maybes))
-solve(S, maybes)
+#maybes = [[[0],[0],[0],[0],[0],[0],[0],[0],[0]],[[0],[0],[0],[0],[0],[0],[0],[0],[0]],[[0],[0],[0],[0],[0],[0],[0],[0],[0]],[[0],[0],[0],[0],[0],[0],[0],[0],[0]],[[0],[0],[0],[0],[0],[0],[0],[0],[0]],[[0],[0],[0],[0],[0],[0],[0],[0],[0]],[[0],[0],[0],[0],[0],[0],[0],[0],[0]],[[0],[0],[0],[0],[0],[0],[0],[0],[0]],[[0],[0],[0],[0],[0],[0],[0],[0],[0]]]
+#print(maybes)
+#Run the code
+ans = solve(S, maybes)
+formatPrint(ans)
 
 
 
